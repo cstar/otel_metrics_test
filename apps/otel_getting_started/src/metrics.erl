4c163f2773d1afb3ac26488787b7bfb455001f03
Eric Cestari <eric@escapevelocity.fr>
2024-08-19 17:48:52 +0200

    Pin to 1.4.0, sample counter, not exported to otel collector

--- apps/otel_getting_started/src/metrics.erl
+++ apps/otel_getting_started/src/metrics.erl
@@ -3,14 +3,9 @@
 -include_lib("opentelemetry_api/include/opentelemetry.hrl").
 -include_lib("opentelemetry_api_experimental/include/otel_meter.hrl").
 %-include_lib("otel_meter.hrl").
--export([start/0]).
+-export([start/0, increase_counter/0]).
 
 start() ->
-    application:ensure_all_started(opentelemetry),
-    application:ensure_all_started(opentelemetry_exporter),
-    application:ensure_all_started(opentelemetry_experimental),
-    application:ensure_all_started(tls_certificate_check),
-    application:ensure_all_started(ssl_verify_fun),
     CounterName = m_counter,
     CounterDesc = <<"macro made counter description">>,
     CounterUnit = kb,
@@ -18,10 +13,8 @@ start() ->
     ?create_counter(CounterName, #{description => CounterDesc,
                                              unit => CounterUnit}),
 
-    ?counter_add(CounterName, 5),
-    ?counter_add(CounterName, 5),
-    ?counter_add(CounterName, 5),
-    ?counter_add(CounterName, 5),
-    ?counter_add(CounterName, 5),
-    ?counter_add(CounterName, 5),
     ?counter_add(CounterName, 5).
+
+increase_counter() ->
+    CounterName = m_counter,
+    ?counter_add(CounterName, 1).
